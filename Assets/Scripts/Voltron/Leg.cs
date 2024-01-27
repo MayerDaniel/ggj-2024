6f309a7b421b4795560a2ef8e68348586d9e1f23
Dan Mayer <dan@mayer.cool>
2024-01-27 14:16:23 -0800

    trying to get robot to lock leg

--- Assets/Scripts/Voltron/Leg.cs
+++ Assets/Scripts/Voltron/Leg.cs
@@ -8,6 +8,16 @@ public class Leg : MonoBehaviour
     [SerializeField]
     Rigidbody2D foot;
 
+    [SerializeField]
+    GameObject pelvis;
+
+    [SerializeField]
+    GameObject thigh;
+
+    [SerializeField]
+    GameObject calf;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +27,43 @@ public class Leg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var gamepad = Gamepad.current;
-        if (gamepad == null)
-            return; // No gamepad connected.
+    
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        if (gamepad.rightTrigger.wasPressedThisFrame)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            foot.AddRelativeForce(Vector3.forward);
+
+            if (!thigh.TryGetComponent<FixedJoint2D>(out FixedJoint2D thighJoint))
+            {
+                thigh.AddComponent<FixedJoint2D>();
+                thigh.GetComponent<FixedJoint2D>().connectedBody = pelvis.GetComponent<Rigidbody2D>();
+            }
+
+            if (!calf.TryGetComponent<FixedJoint2D>(out FixedJoint2D calfJoint))
+            {
+                calf.AddComponent<FixedJoint2D>();
+                calf.GetComponent<FixedJoint2D>().connectedBody = thigh.GetComponent<Rigidbody2D>();
+            }
+
+
+            
+
+            foot.AddRelativeForce(Vector3.forward * 50);
+        } else
+        {
+            Destroy(thigh.GetComponent<FixedJoint2D>());
+            Destroy(calf.GetComponent<FixedJoint2D>());
+
+            Vector3 tempVect = new Vector3(h, v, 0);
+            tempVect = tempVect.normalized * 10 * Time.deltaTime;
+            foot.MovePosition(foot.transform.position + tempVect);
         }
 
-        Vector2 move = gamepad.leftStick.ReadValue();
         
-    }
+  
+
+}
 }
 
 
