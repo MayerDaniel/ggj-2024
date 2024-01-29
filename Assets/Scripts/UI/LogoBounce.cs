75d14edf4e3bc82fb8bc48d4f08aa1c05afa7c3d
Dan Mayer <dan@mayer.cool>
2024-01-28 16:50:09 -0800

    title screen

---
+++ Assets/Scripts/UI/LogoBounce.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoBounce : MonoBehaviour
+{
+    [SerializeField] public float bounceSpeed = 5.0f;
+    [SerializeField] public float bounceHeight = 1.0f;
+
+    private Vector3 originalPosition;
+
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Calculate the vertical movement using Mathf.Sin to create a bouncing effect
+        float verticalMovement = Mathf.Sin(Time.time * bounceSpeed) * bounceHeight;
+
+        // Update the object's position with the vertical movement
+        transform.position = new Vector3(originalPosition.x, originalPosition.y + verticalMovement, originalPosition.z);
+    }
+}
+
+
